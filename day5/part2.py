fca9af4b28690d861f244c7cd9def5dc06c40b59
marzeq <58303665+marzeq@users.noreply.github.com>
2021-12-05 18:03:19 +0100

    Day 5

---
+++ day5/part2.py
@@ -0,0 +1,38 @@
+with open("input.txt", "r") as f:
+    lines = [line.split(" -> ") for line in f.read().split("\n")]
+    for line in lines:
+        line[0] = int(line[0].split(",")[0]), int(line[0].split(",")[1])
+        line[1] = int(line[1].split(",")[0]), int(line[1].split(",")[1])
+
+vents = {}
+
+for line in lines:
+    x0, y0 = line[0]
+    x1, y1 = line[1]
+
+    if x0 == x1:
+        for y in range(min(y0, y1), max(y0, y1) + 1):
+            v = x0, y
+            if v in vents:
+                vents[v] += 1
+            else:
+                vents[v] = 1
+
+    elif y0 == y1:
+        for x in range(min(x0, x1), max(x0, x1) + 1):
+            v = x, y0
+            if v in vents:
+                vents[v] += 1
+            else:
+                vents[v] = 1
+
+    else:
+        for x in range(min(x0, x1), max(x0, x1) + 1):
+            y = y0 + (y1 - y0) * (x - x0) / (x1 - x0)
+            v = x, int(y)
+            if v in vents:
+                vents[v] += 1
+            else:
+                vents[v] = 1
+
+print(len([vent for vent in vents if vents[vent] > 1]))
